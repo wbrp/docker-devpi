4876aec6e77e8df0323c19576645fc89efb0e35f
Dorian Kind <dorian.kind@webrepublic.com>
2016-10-26 11:45:24 +0200

    Update server migration script.

--- devpi/upgrade.sh
+++ devpi/upgrade.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #
 # Replace the server dir with the contents from the server-upgrade dir.
+# Can't just rename the directories as these are docker volumes (results
+# in "Resource busy" errors).
 
-SRC=/devpi/server
-DST=/devpi/server-upgrade
+DST=/devpi/server
+SRC=/devpi/server-upgrade
 
 # Remove old server data
-rm -r $SRC/*
-rm -r $SRC/.*
+find $DST -mindepth 1 -delete
 
 # Move new server data
-mv $DST/* $SRC/
-mv $DST/.* $SRC/
+find $SRC -maxdepth 1 -mindepth 1 -exec mv {} $DST \;
